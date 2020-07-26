e5b99ce7b1115b502a0183e49d2ea3439984f986
Branden Goldenberg <brandengoldenberg@MacBook-Pro.local>
2020-07-26 13:17:09 -0600

    [game] add working jump attack

--- main.js
+++ main.js
@@ -5,8 +5,10 @@ let game;
 let ctx;
 let skier;
 let badGuy;
-let SkierLocateX;
-let SkierLocateY;
+let attack = false;
+let skierLocateX =[];
+let skierLocateY =[];
+let levelCount = 1;
 
 //functions
 const detectHit = () => {
@@ -14,9 +16,20 @@ const detectHit = () => {
         skier.y + skier.height > badGuy.y && 
         skier.x + skier.width > badGuy.x &&
         skier.x < badGuy.x + badGuy.width) {
-            skier.health = skier.health - 1
-            console.log(SkierLocateX)
-            console.log(SkierLocateY)
+        doDamage()
+    }
+}
+
+// detect attack, bounce, get points
+const detectJumpAttack = () => {
+    if( badGuy.y < skier.y + skier.height&&
+        skier.y + skier.height < badGuy.y + 5 && 
+        skier.x + skier.width > badGuy.x &&
+        skier.x < badGuy.x + badGuy.width) {
+        skier.y -= 30
+        badGuy.alive = false
+        skier.points += 10
+
     }
 }
 
@@ -24,44 +37,96 @@ const gameLoop = () => {
     //clear canvas
     ctx.clearRect(0, 0, game.width, game.height)
     //update displays
-    levelDisplay.textContent = `X:${skier.x}\nY:${skier.y}`
+    levelDisplay.textContent = `Level: ${levelCount}`
     healthDisplay.textContent = `Health:${skier.health}`
     pointsDisplay.textContent = `Points:${skier.points}`
     //check if BadGuy is alive
     if (badGuy.alive) {
         // check for collision
+        badGuy.movement()
         badGuy.render()
+        detectJumpAttack()
         detectHit()
+    } else {
+        badGuy = new BadGuy(750, 360)
+    }
+    if (badGuy.x < -badGuy.width) {
+        badGuy = new BadGuy(800, 360)
+    }
+    if (skier.points % 50 === 0 && skier.points !== 0) {
+        levelCount++
     }
     // render skierBro
     skier.render()
     // run gravity
     gravity()
     
+    // track skier location
+    // for (i = 0; i < 5000; i++) {
+    //     if (i === 5000){
+    //         i = 0
+    //     }
+    //     if (i % 100 == false) {
+    //     skierLocateX[i] = skier.x
+    //     skierLocateY[i] = skier.y
+    //     }
+    // }
 }
 
 const gravity = e => {
-    if (skier.y < game.height - skier.height) skier.y += 5
+    if (skier.y < game.height - skier.height) skier.y += 3
 }
 
 const movementHandler = e => {
-
+// console.log(e.keyCode)
     switch (e.keyCode) {
-        case (87): // w - up
-            if (skier.y > 0) skier.y -= 75
+        case (87): // w - jump
+            if (skier.y >= game.height-skier.height) 
+            skier.y -= 75
             break
         case (83):// s - down
-            if (skier.y < game.height - skier.height) skier.y += 5
+            if (skier.y < game.height - skier.height) 
+            skier.y += 3 
             break
-        case (65):// a - left
-            if (skier.x > 0) skier.x -= 10
+        case (65):// a - left 
+            if (skier.x > 0) 
+            skier.x -= 10
+            // canvas.style.backgroundPositionX -= 10
             break
         case (68):// d - right
-            if (skier.x < game.width - skier.width) skier.x += 10
-            break
+            if (skier.x < game.width - skier.width) 
+            skier.x += 10
+        // case (32):// space - attack
+        //     attackForward()
+        //     setTimeout(attackForward, 500)
+        //     break
         // defalut:
         //     console.log('invalid keystroke')
     }
+}
+// build array for test for directionality
+// let test = [1, 1, 1, 3, 3, 3, 5, 5, 5, 5, 7, 7, 7, 7, 7, 9]
+// function findDiffNumbs(array) {
+//     let result = array.filter(e => (e !== )
+//         console.log(result)
+//     })
+// }
+// console.log(findDiffNumbs(test))
+
+function attackForward() {
+    if (!attack){
+        skier.width += 10
+        attack = true
+    } else {
+        skier.width -= 10
+        attack = false
+    }
+}
+
+function doDamage() {
+    skier.health = skier.health - 25
+    // console.log(skierLocateX)
+    // console.log(skierLocateY)
 }
 
 function Skier(x, y, color) {
@@ -90,22 +155,26 @@ function BadGuy(x, y) {
         ctx.fillStyle = this.color
         ctx.strokeRect(this.x, this.y, this.width, this.height)
     }
+    this.movement = function () {
+        this.x = this.x - Math.floor(Math.random() * 5)
+    }
 }
 
 
 
 document.addEventListener('DOMContentLoaded', () => {
-    levelDisplay = document.getElementById('movement')
+    levelDisplay = document.getElementById('level')
     healthDisplay = document.getElementById('hp')
     pointsDisplay = document.getElementById('points')
     game = document.getElementById('game')
+    canvas = document.querySelector('canvas')
     // set some canvas configs
-    game.setAttribute('height', '350px')
+    game.setAttribute('height', '400px')
     game.setAttribute('width', '700px')
     ctx = game.getContext('2d')
 
-    skier = new Skier(325, 300, 'red')
-    badGuy = new BadGuy(500, 310)
+    skier = new Skier(325, 350, 'red')
+    badGuy = new BadGuy(800, 360)
     document.addEventListener('keydown', movementHandler)
 
     let runGame = setInterval(gameLoop, 60)
